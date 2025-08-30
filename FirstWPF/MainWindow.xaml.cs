a35f0ea8ba45deb2fea28847474ed9e07a897705
Dominikishere <vincze.dominik13@gmail.com>
2025-08-30 13:38:02 +0200

    Add project files.

---
+++ FirstWPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace FirstWPF
+{
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow() // code behind
+        {
+            InitializeComponent();
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e) // From UI
+        {
+            if (running) //stop
+            {
+                tbStatus.Text = "The test has been stopped";
+                btnToggleRun.Content = "Run";
+            } else //stop
+            {
+                tbStatus.Text = "The test is running";
+                btnToggleRun.Content = "Stop";
+            }
+            running = !running;
+        }
+    }
+}
